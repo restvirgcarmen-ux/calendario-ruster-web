4e97678e287605801de69cee4bc5132f2af46037
restvirgcarmen-ux <restvirgcarmen@gmail.com>
2026-09-07 22:38:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
 
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
